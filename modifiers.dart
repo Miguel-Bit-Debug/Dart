f42f011a0a2a3d558555cd98c17b7fab6c5761df
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-09-13 23:40:35 -0300

    Add new script dart :man_technologist:

---
+++ modifiers.dart
@@ -0,0 +1,20 @@
+class Valores {
+    static int vezesClicado = 0;
+}
+
+class Pessoa {
+
+}
+
+class Alien {
+
+}
+
+void main() {
+    Valores.vezesClicado = 2;
+    const numero = 3;
+
+    final Pessoa pessoa = Pessoa();
+
+    print(pessoa);
+}
